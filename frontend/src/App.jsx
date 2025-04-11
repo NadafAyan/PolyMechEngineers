4d1ae0a05c5811737d8ebc84ba4073df2a4572da
Ayan Nadaf <nadafayan95@gmail.com>
2025-04-11 23:52:59 +0530

    Added landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './index.css';
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-import About from './components/About';
-import Services from './components/Services';
-import Contact from './components/Contact';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./index.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
