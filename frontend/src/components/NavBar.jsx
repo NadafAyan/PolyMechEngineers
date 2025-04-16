eec5b6d55931c2187921bf8e1318e170a7b4861a
Ayan Nadaf <nadafayan95@gmail.com>
2025-04-16 19:57:43 +0530

    Changes in design

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -12,15 +12,15 @@ const NavBar = () => {
       <div className="flex justify-between items-center px-4 py-3 md:px-10">
         <Link to="/"><img src={logo} alt="PolyMech Engineers" className="h-18 w-auto " /></Link>
 
-        <ul className="flex space-x-14 text-lg font-bold">
+        <ul className="flex space-x-14 text-2xl font-semibold">
           <li>
             <Link to="/home" className="hover:underline hover:decoration-yellow-400 hover:decoration-4 ">HOME</Link>
           </li>
           <li>
-            <Link to="/about" className="hover:underline hover:decoration-yellow-400 hover:decoration-4 ">ABOUT</Link>
+            <Link to="/services" className="hover:underline hover:decoration-yellow-400 hover:decoration-4 ">SERVICES</Link>
           </li>
           <li>
-            <Link to="/services" className="hover:underline hover:decoration-yellow-400 hover:decoration-4 ">SERVICES</Link>
+            <Link to="/about" className="hover:underline hover:decoration-yellow-400 hover:decoration-4 ">ABOUT</Link>
           </li>
           <li>
             <Link to="/contact" className="hover:underline hover:decoration-yellow-400 hover:decoration-4 ">CONTACT</Link>
