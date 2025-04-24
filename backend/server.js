1cab3d8ffdee3c3177b2503ac76a3c6c26ae1810
Ayan Nadaf <nadafayan95@gmail.com>
2025-04-24 09:28:27 +0530

    Created .env file and added twilio credentials to it

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,19 @@ const bodyParser = require("body-parser");
 const twilio = require("twilio");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 
-const accountSid = "AC99b49737cb7f0633a3a4e4ee8fb39979";
-const authToken = "82fb0d2c846f33001a37569cb8f16469";
-const client = new twilio(accountSid, authToken);
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+const receiverNumber = process.env.RECEIVER_PHONE_NUMBER;
+
+const client = require("twilio")(accountSid, authToken);
+
+
+// const accountSid = "AC99b49737cb7f0633a3a4e4ee8fb39979";
+// const authToken = "82fb0d2c846f33001a37569cb8f16469";
+// const client = new twilio(accountSid, authToken);
 
 app.use(bodyParser.json());
 app.use(cors({
@@ -26,11 +35,17 @@ New Contact Form Submission from polymechengineers.com:
 📝 Message: ${message}
   `;
 
+  // client.messages.create({
+  //   from: twilioNumber,
+  //   to: receiverNumber,
+  //   body: messageContent
+  // });
+  
   client.messages
     .create({
       body: text,
-      from: "whatsapp:+14155238886",
-      to: "whatsapp:+919359182996", 
+      from: twilioNumber,
+      to: receiverNumber, 
     })
     .then((message) => {
       console.log("Message sent:", message.sid);
