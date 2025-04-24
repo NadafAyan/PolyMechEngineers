9484fa1a2deec0f8b6ce4395492ea3a2f00cec77
Ayan Nadaf <nadafayan95@gmail.com>
2025-04-24 18:41:15 +0530

    Updated server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const client = require("twilio")(accountSid, authToken);
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://polymechengineers.vercel.app/",
   methods: ["POST"],
   credentials: true,
 }));
